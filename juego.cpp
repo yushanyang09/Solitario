468aac3f05be26f3fe5ea3b5fd2921555d32bdb0
yushnyang09 <yushyang@ucm.es>
2024-03-10 21:49:52 +0100

    main cambiado

--- juego.cpp
+++ juego.cpp
@@ -1,6 +1,32 @@
 #include "juego.h"
+#include "colores.h"
+#include "tablero.h"
+#include <iomanip>
 using namespace std;
+void mostrar(Juego const& juego);
+void pintaCabecera(Juego const& juego);
+void pintaLinea(char esquinaIzda, char cruce, char esquinaDer, Juego const& juego);
+void colorFondo(int color);
+void pintaBordeCelda(int fila, Juego const& juego);
+void pintaCentroCelda(int fila, Juego const& juego);
 
+const char Horizontal = char(196);
+const char UpperLeft = char(218);
+const char UpperCross = char(194);
+const char UpperRight = char(191);
+const char Vertical = char(179);
+const char MidLeft = char(195);
+const char MidCross = char(197);
+const char MidRight = char(180);
+const char LowerLeft = char(192);
+const char LowerCross = char(193);
+const char LowerRight = char(217);
+const char Square = char(219);
+
+const int DEFAULT_COLOR = -1;
+
+
+Direccion obtenerDireccion(int posicion);
 // Públicas
 
 // Procedimiento que crea un juego bloqueado con un tablero vacío
@@ -57,25 +83,45 @@ bool posicionValida(Juego const& juego, int f, int c) {
 
 // Procedimiento que completa el movimiento con las direcciones posibles de la ficha
 void posiblesMovimientos(Juego const& juego, Movimiento& mov) {
-
-	// Variables
-	Direccion listDir[] = {Direccion(0), Direccion(1), Direccion(2), Direccion(3)};
-
-
-	// Se contemplan todas las direcciones y se añaden las posibles
-	// for (pair<int, int> dir: dirs) {}
-
+	mov.cont = 0;
+	for (int i = 0; i < NumDir; i++) {
+		if (mov.fila == dirs[i].first && mov.columna == dirs[i].second) {
+			Direccion dir = obtenerDireccion(i);
+			mov.direcciones[mov.cont] = dir;
+		}
+	}
 
 }
 
 Estado estado(Juego const& juego) {
-
+	return juego.estado;
 }
 
 void jugar(Juego& juego, Movimiento const& mov) {
 
 }
 void mostrar(Juego const& juego) {
+    system("cls"); // borrar consola (clear en Linux)
+	cout << RESET;
+
+	// borde superior
+	pintaCabecera(juego);
+	pintaLinea(UpperLeft, UpperCross, UpperRight,juego);
+
+	// para cada fila
+	for (int fil = 0; fil < juego.tablero.numFilas; fil++) {
+		// primera línea
+		pintaBordeCelda(fil,juego);
+		// segunda línea, con la meta posiblemente
+		pintaCentroCelda(fil,juego);
+		// tercera línea
+		pintaBordeCelda(fil,juego);
+		// separación entre filas
+		if (fil <juego.tablero.numFilas - 1)
+			pintaLinea(MidLeft, MidCross, MidRight,juego);
+		else
+			pintaLinea(LowerLeft, LowerCross, LowerRight,juego);
+	}
 
 
 }
@@ -96,4 +142,82 @@ bool hay_ganador(Juego const& juego) {
 bool hay_movimientos(Juego const& juego) {
 
 
+}
+Direccion obtenerDireccion(int posicion) {
+	switch (posicion) {
+	case 0:
+		return ARRIBA;
+	case 1:
+		return ABAJO;
+	case 2:
+		return IZQUIERDA;
+	case 3:
+		return DERECHA;
+	}
+	return INDETERMINADA;
+}
+
+void pintaCabecera(Juego const& juego) {
+	cout << setw(2) << "    "; // margen inicial
+	cout << setw(5) << 1;
+
+	for (int i = 2; i <= juego.tablero.numColumnas; i++)
+		cout << setw(7) << i;
+
+	cout << '\n';
+}
+
+void pintaLinea(char esquinaIzda, char cruce, char esquinaDer, Juego const& juego) {
+	cout << "    "; // margen inicial
+	cout << esquinaIzda;
+
+	for (int i = 0; i < juego.tablero.numColumnas - 1; i++)
+		cout << string(6, Horizontal) << cruce;
+
+	cout << string(6, Horizontal) << esquinaDer << '\n';
+}
+void colorFondo(int color) {
+	if (color == DEFAULT_COLOR)
+		cout << RESET;
+	else if (color == NULA)
+		cout << BG_BLACK;
+	else if (color == FICHA)
+		cout << BG_LBLUE;
+	else // color == VACIA
+		cout << BG_ORANGE;
+}
+
+
+void pintaBordeCelda(int fila,Juego const& juego) {
+	cout << "    "; // margen inicial
+
+	for (int k = 0; k < juego.tablero.numColumnas; k++) { // cada columna
+		cout << Vertical;
+		colorFondo(juego.tablero.celdas[fila][k]);
+		cout << "      ";
+		colorFondo(DEFAULT_COLOR);
+	}
+
+	cout << Vertical << '\n'; // lateral derecho
+}
+
+void pintaCentroCelda(int fila,Juego const& juego) {
+	cout << "  " << setw(2) << fila + 1; // margen inicial
+
+	for (int k = 0; k < juego.tablero.numColumnas; k++) { // cada columna
+		cout << Vertical;
+		// el color de fondo depende del contenido
+		colorFondo(juego.tablero.celdas[fila][k]);
+
+		if (fila == juego.filaMeta && k == juego.colMeta) { // meta
+			cout << YELLOW;
+			cout << "  " << Square << Square << "  ";
+		}
+		else
+			cout << "      ";
+
+		colorFondo(DEFAULT_COLOR);
+	}
+
+	cout << Vertical << '\n'; // lateral derecho
 }
--- main.cpp
+++ main.cpp
@@ -1,29 +1,47 @@
+
+/*
+* Este es un ejemplo de cómo pintar un tablero constante
+* con el mismo formato que aparece en el enunciado de la
+* práctica.
+
+* Los subprogramas que aparecen a continuación hacen uso de
+* varias constantes, para el tablero y sus dimensiones,
+* o la posición de la meta.
+
+* En la práctica, esos subprogramas seguramente pertenezcan a
+* un módulo con acceso a esos datos del juego que no serán
+* constantes.
+*
+* Y necesitarán parámetro(s).
+*/
+
 #include <iostream>
+#include <fstream>
 #include "tablero.h"
 #include "juego.h"
 #include "movimiento.h"
+ #include "colores.h"
 using namespace std;
 
 // Es posible que esta función pertenezca al módulo tablero
 Movimiento leerMovimiento(Juego solitario);
-void leerPosicion(int &f, int &c);
+void leerPosicion(int& f, int& c);
 
-//comentar
 int main() {
-
-   ifstream archivo;
-
-   Juego solitario;
-	if (cargar(solitario, archivo)) {
-		mostrar(solitario); // se muestra el estado inicial
-		// empezamos a jugar
-		do {
-			Movimiento movimiento = leerMovimiento(solitario);
-			jugar(solitario, movimiento);
-			mostrar(solitario);
-		} while (estado(solitario) == JUGANDO);
-		// mostrar resultado de la partida (ganador o bloqueo)
-	}
+    ifstream archivo;
+    archivo.open("tablero_enunciado");
+     Juego solitario;
+     if (cargar(solitario, archivo)) {
+         mostrar(solitario); // se muestra el estado inicial
+         // empezamos a jugar
+         do {
+             Movimiento movimiento = leerMovimiento(solitario);
+             jugar(solitario, movimiento);
+             mostrar(solitario);
+         } while (estado(solitario) == JUGANDO);
+         // mostrar resultado de la partida (ganador o bloqueo)
+     }
 }
+Movimiento leerMovimiento(Juego solitario) {
 
-
+}
