d65dd2d3812310bfd1a850262ac33b8e17d2d1c6
yushanyang09 <yushyang@ucm.es>
2024-04-29 00:41:16 +0200

    lista medio hecha

---
+++ lista.cpp
@@ -0,0 +1,40 @@
+#include "lista.h"
+
+void redimensionarLista(ListaJuegos& lista,int nuevaCapacidad);
+
+void inicializar(ListaJuegos& lista) {
+	lista.capacidad = TM;
+	lista.cont = 0;
+	lista.datos = new Juego[TM];//el tamaño puede crecer
+}
+void destruir(ListaJuegos& lista) {
+	delete[]lista.datos;
+	lista.datos = nullptr;
+	lista.cont = lista.capacidad = 0;
+}
+
+void insertar(ListaJuegos& lista, Juego& juego) {
+	if (lista.cont == lista.capacidad)
+		redimensionarLista(lista, lista.capacidad * 2);
+	lista.datos[lista.cont] = juego;
+	lista.cont++;
+}
+
+void redimensionarLista(ListaJuegos& lista,int nuevaCapacidad) {
+	Juego* viejo = lista.datos;
+	lista.capacidad = nuevaCapacidad;
+	lista.datos = new Juego[lista.capacidad];
+	for (int i = 0; i < lista.cont; i++)
+		lista.datos[i] = viejo[i];
+	delete[]viejo;
+}
+void eliminar(ListaJuegos& lista, int pos) {
+	for (int i = pos; i < lista.cont-1; i++) {
+		lista.datos[i] = lista.datos[i + 1];
+	}
+	lista.cont--;
+}
+
+Juego& obtener(ListaJuegos& lista, int i) {
+
+}
--- lista.h
+++ lista.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef LISTA_H
+#define LISTA_H
+#include "juego.h"
+const int TM = 5;
+struct ListaJuegos {
+	Juego* datos;
+	int cont;
+	int capacidad;
+};
+void inicializar(ListaJuegos& lista);
+void destruir(ListaJuegos& lista);
+void insertar(ListaJuegos& lista, Juego& juego);
+void eliminar(ListaJuegos& lista, int pos);
+Juego& obtener(ListaJuegos& lista, int i);
+Juego const& obtener(ListaJuegos const& lista, int i);
+int numElems(ListaJuegos const& lista);
+bool esVacia(ListaJuegos const& lista);
+bool cargar(ListaJuegos& lista, std::istream&/*ent/sal*/ entrada);
+void guardar(ListaJuegos const& lista, std::ostream&/*ent/sal*/ salida);
+
+#endif // !LISTA_H
+
